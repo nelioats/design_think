9fc95e519db56653ac7980493dd7cf3413bab816
nelioats <nelioats@gmail.com>
2023-01-04 16:52:01 -0300

    teste

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,13 @@ const handleNav = () => {
         <HiMenuAlt3 onClick={handleNav} className=' text-green-800 cursor-pointer z-20' size={25} />
         <div className={
           nav ? 'ease-in duration-500 fixed text-green-900 left-0 top-0 w-full h-screen bg-white/90 py-7 flex-col z-10' : 
-           'absolute top-0 h-screen ease-in duration-200 z-10 opacity-0'
+           'absolute top-0 h-screen top-[-900%] ease-in duration-500 z-10'
         }> 
+
+{/* <div className={
+          nav ? 'ease-in duration-500 fixed text-green-900 left-0 top-0 w-full h-screen bg-white/90 py-7 flex-col z-10' : 
+           'absolute top-0 h-screen    ease-in duration-200 z-10 opacity-0'
+        }>  */}
             <ul className='flex flex-col fixed w-full  h-full max-h-[550px]  justify-center'>
               <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400'>Empatizar</li>
               <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400'>Definir</li>
