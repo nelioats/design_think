37ff8cb08043bb6b83f42f6fe464f2018011d98b
nelioats <nelioats@gmail.com>
2023-01-05 14:22:16 -0300

    GRIDODS

--- src/components/Ods.jsx
+++ src/components/Ods.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Ods = () => {
   return (
-    <div>Ods</div>
+    <div className='max-w-[1400px] h-[500px] pt-16'>
+      <div className='text-center '>
+        <h2 className='text-xl font-semibold text-orange-500'>Dimensões do TCC: <span className='text-2xl font-bold text-yellow-500'>ODS</span> e Desing Thinking </h2>
+        <div className='grid grid-cols-3 px-4'>
+          <div>
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-1.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-3.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-5.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-7.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-9.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-11.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-13.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-15.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-17.svg" alt="" />
+                
+          </div>
+          <div>
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-2.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-4.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-6.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-8.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-10.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-12.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-14.svg" alt="" />
+                <img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-16.svg" alt="" />
+                <h2>ODS</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
