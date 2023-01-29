861ddc4010052d8a1dd8488978b512f8087c596d
nelioats <nelioats@gmail.com>
2023-01-29 19:39:23 -0300

    testeok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,24 +11,24 @@ const Hero = () => {
           <div className='md:flex md:flex-col-2 md:w-full md:justify-center md:mt-20'>
               <div className='md:text-left md:w-full md:pl-32 '>
                   <div className=''>
-                    <img className='md:w-80 md:h-80 w-64 h-64 md:opacity-80 md:ml-[20%] mb-16' src={logo} alt="" />
+                    <img className=' md:w-72 md:h-72 w-36 h-36 md:opacity-80 md:ml-[20%] md:mt-[5%] mt-[20%] md:mb-16 m-auto' src={logo} alt="" />
                     <div className='md:absolute top-[58%]'>
                       <p>A METODOLOGIA </p>
                       <h1 className='font-bold text-4xl drop-shadow-2xl md:text-7xl'>Design Thinking</h1>
-                      <p className='drop-shadow-2xl text-base md:text-xl'>e Formação de Professores na Orientação do TCC do IEMA</p>
+                      <p className='drop-shadow-2xl text-base md:text-xl px-2'>e Formação de Professores na Orientação do TCC do IEMA</p>
                     </div>
                   </div>
 
               </div>
-              <div className='px-3 space-y-2 md:w-full md:pr-20 md:max-w-lg md:mt-20'>
+              <div className='px-3 space-y-2 md:w-full md:pr-20 md:max-w-lg md:mt-[8%]'>
                 <div> <Link to="empatizar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FD9D24] hover:text-white transition duration-300 ease-out hover:ease-in'>EMPATIZAR</button></Link></div>
                 <div> <Link to="definir" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FD6925] hover:text-white transition duration-300 ease-out hover:ease-in'>DEFINIR</button></Link></div>
                 <div> <Link to="idear" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FCC30B] hover:text-white transition duration-300 ease-out hover:ease-in'>IDEAR</button></Link></div>
                 <div> <Link to="prototipar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#A21942] hover:text-white transition duration-300 ease-out hover:ease-in'>PROTOTIPAR</button></Link></div>
                 <div> <Link to="testar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#0A97D9] hover:text-white transition duration-300 ease-out hover:ease-in'>TESTAR</button></Link></div>
               </div>
-            </div>
-          <div className='w-full space-x-3 space-y-2 mt-2 pt-20'>
+          </div>
+          <div className='w-full space-x-3 space-y-2 md:mt-2 md:pt-20'>
                <a href="https://iema.ma.gov.br/" target={'_blank'} rel='noreferrer'><button className='bg-white text-green-700 font-semibold md:min-w-[300px]'>Saiba + sobre o IEMA</button></a>
               <button className='bg-white text-green-700 font-semibold md:min-w-[300px]'>Biblioteca TCC</button>
           </div>
