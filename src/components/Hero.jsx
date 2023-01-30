565102dd3341281cfa8f48b30e2a5f9b7a8e11b5
nelioats <nelioats@gmail.com>
2023-01-29 22:58:16 -0300

    domingouok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,28 +4,28 @@ import logo from '../img/logo.png'
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen'>
+    <div name='hero' className='w-full h-screen'>
       <img className='top-0 left-0 w-full h-screen object-cover' src="https://images.pexels.com/photos/1250260/pexels-photo-1250260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
       <div className='absolute top-0 left-0 w-full h-screen bg-black/30'></div>
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white text-center'>
           <div className='md:flex md:flex-col-2 md:w-full md:justify-center md:mt-20'>
               <div className='md:text-left md:w-full md:pl-32 '>
                   <div className=''>
-                    <img className=' md:w-72 md:h-72 w-36 h-36 md:opacity-80 md:ml-[20%] md:mt-[5%] mt-[20%] md:mb-16 m-auto' src={logo} alt="" />
+                    <img className=' md:w-72 md:h-72 w-36 h-36 md:opacity-80 md:ml-[20%] md:mt-[5%] mt-[22%] md:mb-16 m-auto' src={logo} alt="" />
                     <div className='md:absolute top-[58%]'>
                       <p>A METODOLOGIA </p>
                       <h1 className='font-bold text-4xl drop-shadow-2xl md:text-7xl'>Design Thinking</h1>
-                      <p className='drop-shadow-2xl text-base md:text-xl px-2'>e Formação de Professores na Orientação do TCC do IEMA</p>
+                      <p className='drop-shadow-2xl text-base md:text-xl px-2 mt-2'>e Formação de Professores na Orientação do TCC do IEMA</p>
                     </div>
                   </div>
 
               </div>
               <div className='px-3 space-y-2 md:w-full md:pr-20 md:max-w-lg md:mt-[8%]'>
-                <div> <Link to="empatizar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FD9D24] hover:text-white transition duration-300 ease-out hover:ease-in'>EMPATIZAR</button></Link></div>
-                <div> <Link to="definir" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FD6925] hover:text-white transition duration-300 ease-out hover:ease-in'>DEFINIR</button></Link></div>
-                <div> <Link to="idear" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#FCC30B] hover:text-white transition duration-300 ease-out hover:ease-in'>IDEAR</button></Link></div>
-                <div> <Link to="prototipar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#A21942] hover:text-white transition duration-300 ease-out hover:ease-in'>PROTOTIPAR</button></Link></div>
-                <div> <Link to="testar" smooth={true} offset={10} duration={500}><button className='w-full hover:bg-[#0A97D9] hover:text-white transition duration-300 ease-out hover:ease-in'>TESTAR</button></Link></div>
+                <div> <Link to="empatizar" smooth={true} offset={0} duration={500}><button className='w-full hover:bg-[#FD9D24] hover:text-white transition duration-300 ease-out hover:ease-in'>EMPATIZAR</button></Link></div>
+                <div> <Link to="definir" smooth={true} offset={0} duration={500}><button className='w-full hover:bg-[#FD6925] hover:text-white transition duration-300 ease-out hover:ease-in'>DEFINIR</button></Link></div>
+                <div> <Link to="idear" smooth={true} offset={0} duration={500}><button className='w-full hover:bg-[#FCC30B] hover:text-white transition duration-300 ease-out hover:ease-in'>IDEAR</button></Link></div>
+                <div> <Link to="prototipar" smooth={true} offset={0} duration={500}><button className='w-full hover:bg-[#A21942] hover:text-white transition duration-300 ease-out hover:ease-in'>PROTOTIPAR</button></Link></div>
+                <div> <Link to="testar" smooth={true} offset={0} duration={500}><button className='w-full hover:bg-[#0A97D9] hover:text-white transition duration-300 ease-out hover:ease-in'>TESTAR</button></Link></div>
               </div>
           </div>
           <div className='w-full space-x-3 space-y-2 md:mt-2 md:pt-8 pt-4'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,16 @@ const handleNav = () => {
         document.body.style.overflow = 'scroll'
       }
 
+      const handleNavLogo = () => {
+        if(nav === true){
+          setNav(!nav)
+          document.body.style.overflow = 'scroll'
+        }
+      }
+
   return (
     <div className='fixed bg-white/90 w-full flex justify-between p-4 items-center z-20 top-0 left-0 right-0 '>
-        <h1 className='text-green-700 font-bold text-2xl z-20 '>DESIGN THINKING</h1>
+         <Link to='hero' smooth={true} offset={0} duration={500} className='z-20' onClick={handleNavLogo}><button className='text-green-700 font-bold text-2xl'>DESIGN THINKING</button></Link>
         <HiMenuAlt3 onClick={handleNav} className=' text-green-800 cursor-pointer z-20' size={25} />
         <div className={
           nav ? 'ease-in duration-75 fixed text-green-900 left-0 top-0 w-full h-screen bg-white/90 py-7 flex-col z-10' : 
@@ -39,11 +46,11 @@ const handleNav = () => {
         }>  */}
             <ul className='flex flex-col fixed w-full  h-full max-h-[550px]  justify-center'>
 
-              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="empatizar" smooth={true} offset={50} duration={500}>Empatizar</Link></li>
-              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="definir" smooth={true} offset={50} duration={500}>Definir</Link></li>
-              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="idear" smooth={true} offset={50} duration={500}>Idear</Link></li>
-              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="prototipar" smooth={true} offset={50} duration={500}>Prototipar</Link></li>
-              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="testar" smooth={true} offset={50} duration={500}>Testar</Link></li>
+              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="empatizar" smooth={true} offset={0} duration={500}>Empatizar</Link></li>
+              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="definir" smooth={true} offset={0} duration={500}>Definir</Link></li>
+              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="idear" smooth={true} offset={0} duration={500}>Idear</Link></li>
+              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="prototipar" smooth={true} offset={0} duration={500}>Prototipar</Link></li>
+              <li className='font-bold text-xl p-6 w-full border-b-2 border-gray-400 cursor-pointer'> <Link onClick={handleClose} to="testar" smooth={true} offset={0} duration={500}>Testar</Link></li>
             </ul>
         </div>
     </div>
