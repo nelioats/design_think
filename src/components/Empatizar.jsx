42e7342070a52c49aa729718d76ea99eb56f0e38
nelioats <nelioats@gmail.com>
2023-01-31 22:58:19 -0300

    todoconteudo

--- src/components/Empatizar.jsx
+++ src/components/Empatizar.jsx
@@ -1,19 +1,49 @@
 import React from 'react'
+import emapatizar01 from '../img/empatizar01.png'
+import emapatizar02 from '../img/empatizar02.png'
 
 const Empatizar = () => {
   return (
     <div name='empatizar' className='bg-[#FD9D24] p-5 mt-12 pb-20 drop-shadow-xl'>
-      <div className='container mx-auto max-w-5xl'>
-          <div className='pt-14 flex flex-col lg:flex-row h-auto md:space-x-6'>
-              <div className='md:w-2/5 mb-4'>
-                <h2 className='text-5xl md:text-7xl text-white font-semibold'>EMPATIZAR</h2>
+      <div className='container mx-auto max-w-6xl'>
+        <h2 className='text-5xl md:text-7xl text-white font-semibold border-b-8 border-white'>EMPATIZAR</h2>
+              
+              <div className=' pt-7 m-auto text-justify'>
+                  <div className=''>
+                      <p className='font-bold'>RESUMO:</p>
+                      <p>A EMPATIA FORNECE O PONTO DE PARTIDA DECISIVO PARA O DESIGN THINKING. O PRIMEIRO ESTÁGIO DO PROCESSO É PARA <span className='text-black font-semibold'>CONHECER</span> E SE COLOCAR NO LUGAR DA <span className='text-black font-semibold'>PERSONA</span> (OU SEU CLIENTE), DA PESSOA QUE <span className='text-black font-semibold'>SENTE</span> AQUELA <span className='text-black font-semibold'>DOR</span>. ENTENDER SUAS NECESSIDADES E DESEJOS. ISSO SIGNIFICA <span className='text-black font-semibold'>OBSERVAR</span> E <span className='text-black font-semibold'>TER</span> CONTATO COM ESSAS PESSOAS PARA COMPREENDÊ-LAS.</p>
+                      <p className='mt-3'>E, COMO ESTAMOS TRABALHANDO COM O TCC BASEADO NOS OBJETIVOS DE DESENVOLVIMENTO SUSTENTÁVEL - <span className='text-black font-semibold'>ODS</span>, IREMOS FAZER TODOS OS PROCESSOS DO DESIGN THINKING SOB ESSA PERSPECTIVA.</p>
+                      <p className='font-bold mt-3'>ENTREGA (S):</p>
+                      <ul>
+                        <ol>LEVANTAMENTO COM DIVERSOS PROBLEMAS/DORES REAIS DA PERSONA (OU CLIENTE).</ol>
+                        <ol>LEMBRE-SE DE PRIORIZAR O CONTATO COM PESSOAS QUE VIVENCIAM ESSE PROBLEMA/DOR.</ol>
+                      </ul>
+                  </div>
+                <div className='pt-4 flex flex-col lg:flex-row h-auto md:space-x-10 font-medium'>
+                    <div className='md:w-1/2 indent-10'>
+                        <p className='pb-4 '>O nosso TCC usa como solução de problema uma abordagem chamada Design Thinking. O Design Thinking é “uma abordagem criativa e focada na resolução de problemas e no ser humano, propicia a descoberta de soluções novas e mais eficazes” (Brown, p. 7). Essa abordagem é divida em 5 etapas, conforme figura abaixo:</p>
+                        <img src={emapatizar01} alt="" />
+                        <p className='pt-4'>A primeira etapa é “empatizar”. Para Rocha (2018,p.162), o “empatizar” nasce da compreensão do problema em seu contexto social. “Na etapa da descoberta/empatia, o ponto principal consiste em compreender o universo no qual a temática do problema está imersa. O problema é investigado a partir do entendimento da realidade (...)” . Em outras palavras,  é necessário identificar as dores / os problemas que afligem nossa realidade. </p>
+                    </div>
+                    <div className='md:w-1/2 indent-10'>
+                        <p>Para chegar aos problemas, podemos usar o Brainstorming como ajuda nesse processo.</p>
+                        <p>Mas o que é Brainstorming? O brainstorming ou tempestade de ideias, mais que uma técnica de dinâmica de grupo, é uma atividade desenvolvida para explorar a potencialidade criativa de um indivíduo ou de um grupo - criatividade em equipe - colocando-a a serviço de objetivos pré-determinados.</p>
+                        <div className='indent-0 m-[4%] p-[6%] border border-white'>
+                          <p>Aqui, sua equipe irá fazer o levantamento de diversas ideias de problemas e depois partir para observar e confirmar se esse problema é uma dor real,ou não, com a persona que vivencia essa situação. </p>
+                        </div>
+                        <div>
+                          <p className='font-bold'>Dicas:</p>
+                          <ul>
+                            <ol>1.	Ter empatia com o público-alvo que sofre com esse problema; </ol>
+                            <ol>2.	Ouvir a equipe/ter ambiente colaborativo;</ol>
+                            <ol>3.	Lembrar que terão de fazer um link com ODS para a realidade local (cidade/município).</ol>
+                          </ul>
+                          <img className='pt-2' src={emapatizar02} alt="" />
+                        </div>
+                    </div>
               </div>
-              <div className='md:w-3/5 p-2 text-justify text-white'>
-                  <p> A EMPATIA FORNECE O PONTO DE PARTIDA DECISIVO PARA O DESIGN THINKING. O PRIMEIRO ESTÁGIO DO PROCESSO É PARA <span className='text-black font-bold'>CONHECER</span> E SE COLOCAR NO LUGAR DA <span className='text-black font-bold'>PERSONA</span> (OU SEU CLIENTE), DA PESSOA QUE <span className='text-black font-bold'>SENTE</span> AQUELA <span className='text-black font-bold'>DOR</span>. ENTENDER SUAS NECESSIDADES E DESEJOS. ISSO SIGNIFICA <span className='text-black font-bold'>OBSERVAR</span> E <span className='text-black font-bold'>TER</span> CONTATO COM ESSAS PESSOAS PARA COMPREENDÊ-LAS.</p>
-                  <p className='mt-3'>E, COMO ESTAMOS TRABALHANDO COM O TCC BASEADO NOS OBJETIVOS DE DESENVOLVIMENTO SUSTENTÁVEL - <span className='text-black font-bold'>ODS</span>, IREMOS FAZER TODOS OS PROCESSOS DO DESIGN THINKING SOB ESSA PERSPECTIVA.</p>
-              </div>
-          </div>
-        </div>
+            </div>
+      </div>
     </div>
   )
 }
--- src/components/Idear.jsx
+++ src/components/Idear.jsx
@@ -1,19 +1,77 @@
 import React from 'react'
+import idear01 from '../img/idear01.png'
+import idear02 from '../img/idear02.png'
 
 const Idear = () => {
   return (
-    <div name='idear' className='bg-[#FCC30B] p-5 pb-16 drop-shadow-xl'>
-      <div className='container mx-auto max-w-5xl'>
-          <div className='pt-14 flex flex-col lg:flex-row h-auto md:space-x-6'>
-              <div className='md:w-2/5 mb-4'>
-                <h2 className='text-5xl md:text-7xl text-white font-semibold'>IDEAR</h2>
+    <div name='idear' className='p-5 pb-16 drop-shadow-xl bg-[#FCC30B]'>
+
+        <div className='container  mx-auto max-w-6xl'>
+        <h2 className='text-5xl md:text-7xl text-white font-semibold border-b-8 border-white'>IDEAR</h2>
+              
+              <div className=' pt-7 m-auto text-justify text-black'>
+                  <div>
+                      <p className='font-bold'>RESUMO:</p>
+                      <p>COM O CONHECIMENTO DE SUA PERSONA E UMA DEFINIÇÃO CLARA DO PROBLEMA EM MENTE, É HORA DE COMEÇAR A TRABALHAR EM <span className='text-black font-semibold'>POTENCIAIS SOLUÇÕES</span>. A TERCEIRA FASE É ONDE A CRIATIVIDADE ACONTECE, E É FUNDAMENTAL QUE ESTA FASE DE IDEAÇÃO/IDEALIZAÇÃO SEJA UMA ZONA LIVRE DE JULGAMENTOS, OU SEJA, PODEM EXPLORAR DIVERSAS OPORTUNIDADES DE SOLUÇÃO.</p> 
+                      <p className='mt-3'>EXISTEM VÁRIAS TÉCNICAS QUE PODEM SER UTILIZADAS AQUI, MAS USAREMOS O <span className=' font-semibold'>MAPA DA EMPATIA</span> E <span className='text-black font-semibold'>MATRIZ CSD</span> PARA NOS AUXILIAR A DESENHAR A IDEIA DA <span className=' font-semibold'>SOLUÇÃO PARA O PROBLEMA</span>. ALÉM, CLARO, DA PESQUISA PARA VALIDARMOS HIPÓTESES E SOLUÇÕES.</p>
+                      <p className='font-bold mt-3'>ENTREGA (S):</p>
+                        <ul>
+                          <ol>1. MAPA DA EMPATIA</ol>
+                          <ol>2. MATRIZ CSD</ol>
+                          <ol>3. PESQUISA</ol>
+                        </ul>
+
+                      <div className='pt-2 font-medium'>
+                        <p>O <span className='text-black font-bold'>Mapa da Empatia</span> é uma ferramenta visual que analisa e descreve, por meio de um diagrama, aspectos comportamentais e o contexto de vida do cliente ideal de um negócio. Com esse recurso, é possível desenhar o cenário, os pensamentos, as ações, os problemas e as necessidades do seu público-alvo. O protagonista do Mapa é um só: sempre o <span className='text-black font-bold'>cliente.</span> Podem ser usuários de aplicativos e consumidores de serviços. Ou seja, é qualquer um que seja atendido pela solução de vocês.</p>
+                        <p className='pt-3'>*** Nesse estágio do <span className='text-black font-bold'>IDEAR</span>, sua equipe irá fazer o <span className='text-black font-bold'>Mapa da Empatia</span>, a <span className='text-black font-bold'>Matriz CSD</span> e o questionário para aplicar a <span className='text-black font-bold'>Pesquisa</span>. *** </p>
+                        
+                        <div class="md:flex md:flex-row pt-3 items-center md:h-screen space-x-2 space-y-2">
+                          <div className='basis-3/4'>
+                            <img src={idear01} alt="" />
+                          </div>
+                          <div className='basis-1/4 text-center border-2 border-white'>
+                            <p className='p-4'>O Mapa da Empatia é formado por 6 quadrantes que terão como foco o seu cliente, a pessoa que precisa da solução da empresa de vocês.</p>
+                          </div>   
+                        </div>
+                      </div>
+                  </div>
+                <div className='pt-4 flex flex-col lg:flex-row h-auto md:space-x-10 font-medium'>
+                    <div className='md:w-1/2 indent-8'>
+                        <p className='pb-4 '>•	E o que é a Matriz CSD? A Matriz de Certezas, Suposições e Dúvidas — também conhecida carinhosamente como Matriz CSD — é um framework que busca conectar pontos, organizar informações e, principalmente, dar um norte para um barco à deriva.</p>
+                        
+                        <p className='pt-3'>A Matriz CSD é muito útil para dar o pontapé inicial em projetos, mas também é uma ferramenta importante para acelerar o processo de discovery/descoberta. </p>
+                        <p className='pt-4 pb-2'>Pode ser utilizado o modelo abaixo:</p>
+                        <img src={idear02} alt="" />
+                        <p className='pt-4 pb-2'>Feita a Matriz CSD, você poderá montar o questionário da Pesquisa. E é de extrema importância conter, nesse questionário, as Suposições e Dúvidas que a equipe precisa conhecer.</p>
+                    </div>
+                    <div className='md:w-1/2 indent-0'>
+                        <p className='font-semibold pb-2'>Algumas dicas na hora de montar o questionário:</p>
+                        <ul>
+                            <ol>1.	Identifique o que você deseja cobrir na pesquisa.</ol>
+                            <ol>2.	A clareza no assunto tem maior importância, pois é o primeiro passo na elaboração da pesquisa.</ol>
+                            <ol>3.	Escolha as palavras corretas: É essencial que as palavras ou frases que você usa, ao criar o questionário, sejam fáceis de entender.</ol>
+                            <ol>4.	Faça apenas uma pergunta por vez.</ol>
+                            <ol>5.	Seja flexível com suas opções: Às vezes, o entrevistado não deseja necessariamente escolher entre as opções de resposta que foram fornecidas pelo criador da pesquisa. Em uma situação assim, é interessante ter o tópico “outra” opção.</ol>
+                            <ol>6.	Perguntas abertas ou fechadas? Considere a finalidade e dê as opções mais adequadas de resposta.</ol>
+                            <ol>7.	É importante conhecer o seu público para fazer um questionário: Por exemplo, se o público estudado fala a língua espanhola, o envio das pesquisas precisa ser feito no idioma adequado.</ol>
+                            <ol>8.	Escolha bem a ferramenta: Uma opção gratuita e muito utilizada é o Google Forms, ou apenas Formulário do Google.</ol>
+
+                        </ul>
+
+                        <p className='pt-3 font-semibold'>Vamos voltar ao exemplo da nossa amiga Larissa Fernandes? Então, a equipe que está buscando uma solução para o problema da Larissa fez o seguinte: </p>
+                        <ul className='pt-2'>
+                          <li>•	Construiu o Mapa de Empatia, ou seja, buscou saber: O que Larissa vê? O que Larissa ouve? O que Larissa fala? Quais são as dores de Larissa Fernandes? Quais são suas necessidades?</li>
+                          <li>•	Produziu a Matriz CSD;</li>
+                          <li>•	Depois, elaborou e aplicou um questionário na comunidade que sofria com o odor de resíduo animal.</li>
+                        </ul>
+                        <p className='pt-2'>Após análise do Mapa de Empatia, Matriz CSD e Resultado e <span className='italic'> brainstorming sobre a melhor solução para o problema, chegou-se à seguinte solução: “Criar um biodigestor de baixo custo para reduzir poluição de resíduo animal na natureza, no município de Coroatá, e, assim, diminuir o impacto na vida da comunidade”</span>. Se, porventura, essa ideia for aceita pela comunidade, avança-se para a fase seguinte. Caso contrário, busca-se uma nova ideia. </p>
+                    </div>
               </div>
-              <div className='md:w-3/5 p-2 text-justify text-white'>
-                <p>COM O CONHECIMENTO DE SUA PERSONA E UMA DEFINIÇÃO CLARA DO PROBLEMA EM MENTE, É HORA DE COMEÇAR A TRABALHAR EM <span className='text-black font-bold'>POTENCIAIS SOLUÇÕES</span>. A TERCEIRA FASE É ONDE A CRIATIVIDADE ACONTECE, E É FUNDAMENTAL QUE ESTA FASE DE IDEAÇÃO/IDEALIZAÇÃO SEJA UMA ZONA LIVRE DE JULGAMENTOS, OU SEJA, PODEM EXPLORAR DIVERSAS OPORTUNIDADES DE SOLUÇÃO.</p> 
-                <p className='mt-3'>EXISTEM VÁRIAS TÉCNICAS QUE PODEM SER UTILIZADAS AQUI, MAS USAREMOS O <span className='text-black font-bold'>MAPA DA EMPATIA</span> E <span className='text-black font-bold'>MATRIZ CSD</span> PARA NOS AUXILIAR A DESENHAR A IDEIA DA <span className='text-black font-bold'>SOLUÇÃO PARA O PROBLEMA</span>. ALÉM, CLARO, DA PESQUISA PARA VALIDARMOS HIPÓTESES E SOLUÇÕES.</p>  
-              </div>
-          </div>
-        </div>
+            </div>
+      </div>
+
+
+
     </div>
   )
 }
